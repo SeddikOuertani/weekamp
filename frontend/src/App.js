996b3042316867c7175bef03d0b4f77e4be31ed5
SeddikOuertani <mohamedseddikoeurtani@gmail.com>
2022-10-06 21:22:48 +0100

    making navbar animation only work in home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,62 +1,56 @@
-import React, { Component, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useDispatch } from "react-redux";
+import { useLocation } from "react-router-dom";
 import "./App.css";
 import Main from "./layout/main";
 import Navbar from "./layout/navbar";
 
 //Lazily loaded into memory
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoggedIn: true,
-      blockInvertedRef: React.createRef(null),
-      pathname: window.location.pathname,
-    };
-  }
+const App = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const { pathname } = useLocation();
+  const blockInvertedRef = useRef(null);
 
-  runNavbarAnimation(toggle) {
+  const runNavbarAnimation = (toggle) => {
     let navbarElement = document.getElementById("navbar-backdrop");
     if (toggle) {
       navbarElement.classList.add("animation");
     } else {
       navbarElement.classList.remove("animation");
     }
-  }
+  };
 
-  addingScrollListener() {
-    window.addEventListener("scroll", () => {
-      if (
-        (window.scrollY >= 0 && window.scrollY < 50) ||
-        window.scrollY >= 50 + window.innerHeight
-      ) {
-        this.runNavbarAnimation(true);
-      } else {
-        this.runNavbarAnimation(false);
-      }
-    });
-  }
+  const addingScrollListener = () => {
+    if (pathname === "/home") {
+      window.addEventListener("scroll", () => {
+        if (
+          (window.scrollY >= 0 && window.scrollY < 50) ||
+          window.scrollY >= 50 + window.innerHeight
+        ) {
+          runNavbarAnimation(true);
+        } else {
+          runNavbarAnimation(false);
+        }
+      });
+    }
+  };
 
-  componentDidMount() {
-    this.addingScrollListener();
-  }
+  useEffect(() => {
+    addingScrollListener();
+  }, [pathname]);
 
-  componentDidUpdate() {}
-
-  render() {
-    return (
-      <div className="App">
-        {this.state.isLoggedIn ? (
-          <div className="layout">
-            <Navbar Id="navbar-backdrop" />
-            {/* <Aside /> */}
-            <Main BlockInvertedRef={this.blockInvertedRef} />
-          </div>
-        ) : null}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="App">
+      {isLoggedIn ? (
+        <div className="layout">
+          <Navbar Id="navbar-backdrop" />
+          {/* <Aside /> */}
+          <Main BlockInvertedRef={blockInvertedRef} />
+        </div>
+      ) : null}
+    </div>
+  );
+};
 
 export default App;
